2c8ba9dcf97706536adbd238d1f5833c7b6916e0
jmsjcmc <jecemecojamestabilog@gmail.com>
2025-01-06 19:23:32 +0800

    Update: Fix logout bug

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -59,7 +59,15 @@
                                 <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Earnings</a>
                               </li>
                               <li>
-                                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Sign out</a>
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+
+                                    <x-dropdown-link :href="route('logout')"
+                                            onclick="event.preventDefault();
+                                                        this.closest('form').submit();">
+                                        {{ __('Log Out') }}
+                                    </x-dropdown-link>
+                                </form>
                               </li>
                             </ul>
                           </div>
